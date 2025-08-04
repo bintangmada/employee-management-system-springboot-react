da0ad00fd6852641191b5948644e99eb0d62edcb
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-08-04 14:32:35 +0700

    get all employee from api

--- ems-frontend-react/src/services/EmployeeService.js
+++ ems-frontend-react/src/services/EmployeeService.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const REST_API_BASE_URL = "http://localhost:8033/api/employee";
 
 export const listEmployees = () => {
-  return axion.get(REST_API_BASE_URL + "/get-all");
+  return axios.get(REST_API_BASE_URL + "/get-all");
 };
