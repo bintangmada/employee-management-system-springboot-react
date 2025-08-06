3f4a239bb3968341fa82060295374770fa36dd76
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-08-06 09:39:19 +0700

    add create employee axios

--- ems-frontend-react/src/services/EmployeeService.js
+++ ems-frontend-react/src/services/EmployeeService.js
@@ -5,3 +5,7 @@ const REST_API_BASE_URL = "http://localhost:8033/api/employee";
 export const listEmployees = () => {
   return axios.get(REST_API_BASE_URL + "/get-all");
 };
+
+export const createEmployee = (employee) => {
+  return axios.post(REST_API_BASE_URL, employee);
+};
